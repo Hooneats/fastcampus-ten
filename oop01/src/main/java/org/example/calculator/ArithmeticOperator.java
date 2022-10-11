f5dfb05696da0e01e1590ef2b7c26e7ac1f82ad4
hooneats <hooneats@naver.com>
2022-10-11 23:21:37 +0900

    enum 타입으로 리팩토링

---
+++ oop01/src/main/java/org/example/calculator/ArithmeticOperator.java
@@ -0,0 +1,49 @@
+package org.example.calculator;
+
+import java.util.Arrays;
+
+public enum ArithmeticOperator {
+    ADDITION("+"){
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 + operand2;
+        }
+    },
+    SUBTRACTION("-") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 - operand2;
+        }
+    },
+    MULTIPLICATION("*") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 * operand2;
+        }
+    },
+    DIVISION("/") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 / operand2;
+        }
+    };
+
+    private final String operator;
+
+    ArithmeticOperator(String operator) {
+        this.operator = operator;
+    }
+
+    protected abstract int arithmeticCalculate(final int operand1, final int operand2);
+
+    // 외부에 노출
+    public static int calculate(int operand1, String operator, int operand2) {
+        ArithmeticOperator arithmeticOperator = Arrays.stream(values())
+                .filter(value -> value.operator.equals(operator))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙 연산이 아닙니다."));
+
+        int result = arithmeticOperator.arithmeticCalculate(operand1, operand2);
+        return result;
+    }
+}
