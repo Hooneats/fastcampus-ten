a1306b78a4e94dcef24543da691f70e486a2ef9e
hooneats <hooneats@naver.com>
2022-10-11 23:34:49 +0900

    인스턴스를 사용해서 리팩토링

--- oop01/src/main/java/org/example/calculator/Calculator.java
+++ oop01/src/main/java/org/example/calculator/Calculator.java
@@ -1,9 +1,23 @@
 package org.example.calculator;
 
+import org.example.calculator.calcutateInterface.*;
+
+import java.util.List;
+
 public class Calculator {
+
+    private static final List<NewArithmeticOperator> arithmeticOperators =
+            List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
 
 
     public static int calculate(int operand1, String operator, int operand2) {
-        return ArithmeticOperator.calculate(operand1, operator, operand2);
+        // enum 사용
+//        return ArithmeticOperator.calculate(operand1, operator, operand2);
+        // interface 사용
+        return arithmeticOperators.stream()
+                .filter(arithmeticOperators -> arithmeticOperators.supports(operator))
+                .findFirst()
+                .map(arithmeticOperator -> arithmeticOperator.calculate(operand1, operand2))
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
     }
 }
