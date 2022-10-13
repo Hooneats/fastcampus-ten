cb2b8046338d00aed191ce9c99d945fc6e12ef75
hooneats <hooneats@naver.com>
2022-10-13 23:49:09 +0900

    jdbc

---
+++ jdbc-practice/src/main/java/org/example/JdbcTemplate.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcTemplate {
+
+
+    public void executeUpdate(User user, String sql, PreparedStatementSetterFunction pss) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+            pss.setter(pstmt);
+
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public <T> T executeQuery(String sql, PreparedStatementSetterFunction pss, RowMapper rowMapper) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+            pss.setter(pstmt);
+
+            rs = pstmt.executeQuery();
+
+            T user = null;
+            if (rs.next()) {
+                user = (T) rowMapper.map(rs);
+            }
+            return user;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+}
