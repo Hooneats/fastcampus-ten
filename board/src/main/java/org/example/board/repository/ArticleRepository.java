16b482ec751380a69a7fb58ca55455dce732f583
hooneats <hooneats@naver.com>
2022-11-22 16:20:50 +0900

    board QueryDSL

--- board/src/main/java/org/example/board/repository/ArticleRepository.java
+++ board/src/main/java/org/example/board/repository/ArticleRepository.java
@@ -1,9 +1,33 @@
 package org.example.board.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.example.board.domain.Article;
+import org.example.board.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        // QuerydslPredicateExecutor 에 의해 모든 필드가 열려있다. -> 선택적인 필드만 열리게끔 변경
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createAt, root.createdBy);
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // 대소문자 구분없이 like '${v} -> % 넣는것을 해줘야함
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // 대소문자 구분없이 like '%${v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // 대소문자 구분없이 like '%${v}%'
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // 대소문자 구분없이 like '%${v}%'
+        bindings.bind(root.createAt).first(DateTimeExpression::eq); // 대소문자 구분없이 like '%${v}%'
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase); // 대소문자 구분없이 like '%${v}%'
+    }
 }
